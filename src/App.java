59e5800de3e7ae1e7344516c6d9d13cee9be2257
Victor Ambrozi <victorambrozi.dev@gmail.com>
2022-10-21 11:20:26 -0300

    Cria conexão com banco de dados

--- src/App.java
+++ src/App.java
@@ -1,5 +1,15 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class App {
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        ConexaoDB con = new ConexaoDB();
+
+        con.open();
+        con.close();
     }
 }
---
+++ src/ConexaoDB.java
@@ -0,0 +1,37 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class ConexaoDB {
+  Connection connection;
+
+  public void open() throws Exception {
+    String url = "jdbc:mysql://localhost:3306/cadastros";
+    String user = "root";
+    String password = "root";
+    String driverName = "com.mysql.cj.jdbc.Driver";
+
+    try {
+      Class.forName((driverName)); // inicia o driver .jar
+      connection = DriverManager.getConnection(url, user, password); // manda as informações pra conexão do bando de
+                                                                     // dados
+      System.out.println("Sucesso! Conexão estabelecida...");
+    } catch (SQLException | ClassNotFoundException e) {
+      System.out.println(e);
+    }
+
+  }
+
+  public void close() throws Exception {
+    try {
+      connection.close();
+      System.out.println("Sucesso! Conexão encerrada...");
+      
+    } catch (SQLException | ClassCastException e) {
+      System.out.println("Error: " + e);
+    }
+
+  }
+}
