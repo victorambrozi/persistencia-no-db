b4b6be510863f2d71eec90391f4dcb0398eb3b7e
victorambrozi <victorambrozi.dev@gmail.com>
2022-10-22 11:46:34 -0300

    CRUD - select ok

--- src/App.java
+++ src/App.java
@@ -1,16 +1,11 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
 public class App {
 
     public static void main(String[] args) throws Exception {
         FormsDao formDB = new FormsDao();
-        Contato joaoPaulo = new Contato(1, "João Paulo", "joaopaulo@formulario.com");
+        // Contato joaoPaulo = new Contato(1, "João Paulo", "joaopaulo@formulario.com");
         Contato duda = new Contato(2, "Eduarda", "duda@formulario.com");
-        formDB.incluir(duda);
+        // formDB.incluir(duda);
+        System.out.println(formDB.selecionarTodos());
 
     }
 }
--- src/FormsDao.java
+++ src/FormsDao.java
@@ -1,21 +1,66 @@
-import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.mysql.cj.protocol.Resultset;
 
 public class FormsDao extends ConexaoDao {
 
-    
     public void incluir(Contato contact) throws Exception {
         String sql = "INSERT INTO contatos VALUES(?, ?, ?)";
-        open();
-        statement = connection.prepareStatement(sql);
-        statement.setInt(1, contact.getId());
-        statement.setString(2, contact.getNomeContato());
-        statement.setString(3, contact.getEmailContato());
-        statement.execute();
-        statement.close();
-        close();
+
+        try {
+            open();
+            statement = connection.prepareStatement(sql);
+            statement.setInt(1, contact.getId());
+            statement.setString(2, contact.getNomeContato());
+            statement.setString(3, contact.getEmailContato());
+            statement.execute();
+            statement.close();
+            close();
+        } catch (Exception error) {
+            System.out.println("Error: FormsD" + error);
+        }
+
     }
-    public void selecionarTodos() {}
-    public void selecionarUsuario() {}
-    public void deletar() {}
-    public void atualizar() {}
+
+    public List<Contato> selecionarTodos() throws Exception {
+        List<Contato> contatoList = new ArrayList<>();
+        String sql = "SELECT * FROM contatos";
+
+        try {
+            open();
+            statement = connection.prepareStatement(sql);
+            result = statement.executeQuery();
+
+            while(result.next()) {
+                Contato contato = new Contato();
+
+                contato.setId(result.getInt(1));
+                contato.setNomeContato(result.getString(2));
+                contato.setEmailContato(result.getString(3));
+
+                contatoList.add(contato);
+            }
+
+
+            statement.close();
+            close();
+        } catch(Exception error) {
+            System.out.println("Error: FormsD" + error);
+        }
+
+        return contatoList;
+
+    }
+
+    public void selecionarUsuario() {
+    }
+
+    public void deletar() {
+    }
+
+    public void atualizar() {
+    }
 }
